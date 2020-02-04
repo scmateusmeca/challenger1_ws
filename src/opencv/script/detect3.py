4eba0ad0fa90f79a4f7092e98148e1a0a97dbedf
Mateus S Cerqueira <scmateusmeca@gmail.com>
2020-02-04 08:52:06 -0300

    vai

--- src/opencv/script/detect3.py
+++ src/opencv/script/detect3.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python 
 
 # libraries:
+import os
 import cv2
 import math
 import time
 import rospy
-import os
 import numpy as np
 from std_msgs.msg import Int32
 from std_msgs.msg import String
-from sensor_msgs.msg import Image, CameraInfo
+from controller import Controller
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image
+from actionlib_msgs.msg import GoalID 
+from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist, Vector3
 from cv_bridge import CvBridge, CvBridgeError
 from move_base_msgs.msg import MoveBaseActionGoal
-from geometry_msgs.msg import PoseStamped
 from nav2d_navigator.msg import GetFirstMapActionGoal, ExploreActionGoal
-from actionlib_msgs.msg import GoalID 
-
 
 class Camera:
-  odometry_data = None
-  mission_phase = None
-  camera_info = None
-  msg_move_to_goal = None
-  flag = None
-  timer_flag = None
-  flag_kill = False
-
   def __init__(self):
-     # focal length
-    self.focalLength = 937.8194580078125
+    self.cont = 0
+    # node killer
+    self.kill = False
+    # flag var
+    self.flag1 = True
+    self.flag2 = True
+    self.flag3 = True
+    # save img var
+    self.save_img = True
+    # end mission var
+    self.end_mission = True
+    # focal length
+    self.focalLength = 838.9544
     # bridge object to convert cv2 to ros and ros to cv2
     self.bridge = CvBridge()
     # timer var
@@ -40,38 +43,32 @@ class Camera:
     # controllers
     self.linear_vel_control = Controller(5, -5, 0.01, 0, 0)
     self.angular_vel_control = Controller(5, -5, 0.01, 0, 0)
-    # odometry topic subscription
-    rospy.Subscriber('/odometry/filtered', Odometry, self.callback_odometry)
     # image publisher object
     self.image_pub = rospy.Publisher('camera/mission', Image, queue_size=10)
-    # cmd_vel publisher object
+    # cmd_vel publisher object for flag3 adjustment
     self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    # get camera info
-    rospy.Subscriber("/diff/camera_top/camera_info", CameraInfo, self.callback_camera_info)
-    # move to goal 
-    self.pub_move_to_goal = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
-    self.msg_move_to_goal = PoseStamped()
-    self.flag = True
-    self.camera_info = CameraInfo()
+    # move_base publisher object
+    self.move_base_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1) 
+    # exploration setup
     self.start_map = rospy.Publisher("/GetFirstMap/goal", GetFirstMapActionGoal, queue_size=1)
     self.start_explore = rospy.Publisher("/Explore/goal", ExploreActionGoal, queue_size = 1)
     self.cancel_map = rospy.Publisher("/GetFirstMap/cancel", GoalID, queue_size = 1)
     self.cancel_explore = rospy.Publisher("/Explore/cancel", GoalID, queue_size = 1)
+    # basic map setup
     time.sleep(1)
     self.start_map.publish()
     time.sleep(5)
     self.cancel_map.publish()
     time.sleep(2)
     self.start_explore.publish()
-
-
+    print('GO')
 
   def callback(self, data):
     # setup timer and font
     timer = int(time.time() - self.start)
     font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # convert img to cv2
+    # convert ros img to cv2
     cv2_frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
 
     ### COLOR DETECTION ###
@@ -104,121 +101,90 @@ class Camera:
       centers.append(aux1)
       radius.append(aux2)
       ## if the camera find the sphere ##
-      if(len(contours_poly[index]) > 12):
+      if(len(contours_poly[index]) > 10):        
         # draw a circle in sphere and put a warning message
         cv2.circle(cv2_frame, (int(centers[index][0]), int(centers[index][1])), int(radius[index]), (0, 0, 255), 5) 
         cv2.putText(cv2_frame, 'BOMB HAS BEEN DETECTED!', (20, 130), font, 2, (0, 0, 255), 5)
-        # controller actions
-        linear_vel =  0 #self.linear_vel_control.calculate(1, 174, radius[0])
-        angular_vel = self.angular_vel_control.calculate(1, 640, centers[0][0])
-        #self.cmd_vel_pub(linear_vel, angular_vel, cv2_frame) 
-        # print info on terminal
-        print('CONTROL INFO :')
-        print('radius: ' + str(radius[0]))
-        print('center x position: ' + str(centers[0][0]))
-        #print('linear vel: ' + str(linear_vel))                
-        #print('angular vel: ' + str(angular_vel))
+        if not self.kill:       
+          os.system('rosnode kill Operator')
+          self.kill = True
+        
+        ### MOVE BASE GOAL ###
         self.goal_move_base(centers[0][0], radius[0])
-        print('##################################')
+            
     # merge timer info to frame
     cv2.putText(cv2_frame, str(timer) + 's', (20, 60), font, 2, (50, 255, 50), 5) 
     cv2.putText(cv2_frame, str(time.ctime()), (10, 700), font, 2, (50, 255, 50), 6)
+
+    # merge end mission information and save a jpg file
+    if self.end_mission == False:
+      cv2.putText(cv2_frame, 'MISSION ACCOMPLISHED', (20, 220), font, 2, (255, 0, 0), 5)
+      # save img
+      if self.save_img:
+        cv2.imwrite('end_mission.jpg', cv2_frame)
+        self.save_img = False
 
     # convert img to ros and pub image in a topic
     ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
     self.image_pub.publish(ros_frame)
 
-  def callback_odometry(self, data):
-    self.odometry_data = data
-
-  def callback_camera_info(self, data):
-    self.camera_info = data
-
   def listener(self):
     # subscribe to a topic
-    rospy.Subscriber('/diff/camera_top/image_raw', Image, self.callback)  
+    rospy.Subscriber('camera/image_raw', Image, self.callback)  
     # simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-  def cmd_vel_pub(self, linear, angular, frame):
-    cv2.putText(frame, 'Process: center alignment', (20, 640), cv2.FONT_HERSHEY_SIMPLEX, 2, (200, 0, 0), 3)
+  def cmd_vel_pub(self, linear, angular):
     vel_msg = Twist()
     vel_msg.linear.x = linear
     vel_msg.angular.z = angular
     self.velocity_publisher.publish(vel_msg)
   
   def goal_move_base(self, center_ball, radius):
+    # distance calculation
     distance = (1 * self.focalLength) / (radius * 2)
-    # if distance < 6.0:
-    #   self.flag_kill = True
-    #   self.cancel_explore.publish()
-    #   self.cancel_map.publish()
-    #   os.system("rostopic pub /move_base/cancel actionlib_msgs/GoalID -- {}")
-    #   tws = Twist()
-    #   tws.linear.x = 0.0
-    #   tws.angular.z = 0.0
-    #   self.velocity_publisher.publish(tws)
-    #   print('Esfera a 6.0 m de distancia maxima')
-    y_move_base = -(center_ball - self.camera_info.width/2) / (radius*2) 
+    y_move_base = -(center_ball - 640) / (radius*2) 
     if abs(y_move_base) < 0.006:
       x_move_base = distance
     else:
       x_move_base = math.sqrt(distance**2 - y_move_base**2)
-    self.msg_move_to_goal.pose.position.x = x_move_base
-    self.msg_move_to_goal.pose.position.y = y_move_base
-    self.msg_move_to_goal.pose.orientation.w = 1
-    self.msg_move_to_goal.header.frame_id = self.camera_info.header.frame_id
-    if self.flag:
-      self.cancel_explore.publish()
-      self.cancel_map.publish()
-      os.system("rosnode kill /Operator")
-      print('enviando goal')
-      self.pub_move_to_goal.publish(self.msg_move_to_goal)
-      self.flag = False
-      self.timer_flag = time.time()
-    if time.time() - self.timer_flag > 10:
-      self.flag = True      
-    print('distance to sphere: ' + str(distance))
-    print('INCREMENTO X: ' + str(x_move_base))
-    print('INCREMENTO Y: ' + str(y_move_base))      
-
-  def pub_move_base(self, x, y):
-    if self.mission_phase == None:
-      self.mission_phase = 1
-
-  #def move_base_pub(self, x, y, angle):
-    #coment
-
-class Controller:
-  sat_max = 0
-  sat_min = 0
-  kp = 0
-  ki = 0
-  kd = 0
-  error_integral = 0 
-  error_prev = 0 
-
-  def __init__ (self, sat_max, sat_min, kp, ki, kd):
-    self.sat_max = sat_max 
-    self.sat_min = sat_min 
-    self.kp = kp 
-    self.ki = ki 
-    self.kd = kd 
     
-  def calculate(self, time, setpoint, process):
-    # set the error
-    self.error = setpoint - process
-    self.error_integral =+ self.error
-    # calculate the output
-    control_output = self.kp*self.error + self.ki*(self.error_integral)*time + self.kd*(self.error - self.error_prev)/time    
-    # using saturation max and min in control_output 
-    if (control_output > self.sat_max):
-      control_output = self.sat_max
-    elif (control_output < self.sat_min):
-      control_output = self.sat_min
-    # set error_prev for kd   
-    self.error_prev = self.error   
-    return control_output  
+    # setup pub values with x and y positions
+    msg_move_to_goal = PoseStamped()
+    msg_move_to_goal.pose.position.x = x_move_base  
+    msg_move_to_goal.pose.position.y = y_move_base
+    msg_move_to_goal.pose.orientation.w = 1
+    msg_move_to_goal.header.frame_id = 'kinect_link'
+
+    self.cont +=1
+    # pub values on move_base or use controller for best position
+    # if self.flag1 and distance > 30 and sel:
+    #   self.move_base_pub.publish(msg_move_to_goal)
+    #   self.flag1 = False
+    if self.cont == 600:
+      self.move_base_pub.publish(msg_move_to_goal)
+      self.flag2 = False
+      self.cont = 0
+
+    elif distance < 5:
+      self.flag3 = False
+      if (172 < radius < 176) and (638 < center_ball < 642):
+        self.end_mission = False 
+      # controller actions
+      linear_vel = self.linear_vel_control.calculate(1, 174, radius)
+      angular_vel = self.angular_vel_control.calculate(1, 640, center_ball)
+      self.cmd_vel_pub(linear_vel, angular_vel)
+      
+    # print information for debug
+    print('DEBUG:')
+    print('DISTANCIA EM LINHA: ' + str(distance))
+    print('INCREMENTO X: ' + str(x_move_base))
+    print('INCREMENTO Y: ' + str(y_move_base))
+    print('DET: ' + str(self.flag2))
+    print('ADJUST: ' + str(self.flag3))
+    print('RAIO: ' + str(radius))
+    print('POSICAO DO CENTRO:' + str(center_ball))
+    print('##################################')
 
 # main function
 if __name__	== '__main__':
