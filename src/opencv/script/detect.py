d8844e6852e4812bf14464c48daad0dba856c64e
Mateus S Cerqueira <scmateusmeca@gmail.com>
2020-03-03 09:18:13 -0300

    novo

--- src/opencv/script/detect.py
+++ src/opencv/script/detect.py
@@ -40,7 +40,7 @@ class Camera:
     self.linear_vel_control = Controller(5, -5, 0.01, 0, 0)
     self.angular_vel_control = Controller(5, -5, 0.01, 0, 0)
     # odometry topic subscription
-    rospy.Subscriber('/odometry/filtered', Odometry, self.callback_odometry)
+    #rospy.Subscriber('/odometry/filtered', Odometry, self.callback_odometry)
     # image publisher object
     self.image_pub = rospy.Publisher('camera/mission', Image, queue_size=10)
     # cmd_vel publisher object
@@ -52,15 +52,23 @@ class Camera:
     self.msg_move_to_goal = PoseStamped()
     self.flag = True
     self.camera_info = CameraInfo()
+   
     self.start_map = rospy.Publisher("/GetFirstMap/goal", GetFirstMapActionGoal, queue_size=1)
     self.start_explore = rospy.Publisher("/Explore/goal", ExploreActionGoal, queue_size = 1)
     self.cancel_map = rospy.Publisher("/GetFirstMap/cancel", GoalID, queue_size = 1)
     self.cancel_explore = rospy.Publisher("/Explore/cancel", GoalID, queue_size = 1)
+   
     time.sleep(1)
     self.start_map.publish()
     time.sleep(5)
     self.cancel_map.publish()
     time.sleep(2)
+    self.start_explore.publish()
+    self.msg_move_to_goal.pose.position.x = 20
+    self.msg_move_to_goal.pose.position.y = 0
+    self.msg_move_to_goal.pose.orientation.w = 1
+    self.msg_move_to_goal.header.frame_id = 'base_link'#self.camera_info.header.frame_id
+    self.pub_move_to_goal.publish(self.msg_move_to_goal)
     self.start_explore.publish()
 
 
@@ -106,7 +114,7 @@ class Camera:
       if(len(contours_poly[index]) > 10):
         # draw a circle in sphere and put a warning message
         cv2.circle(cv2_frame, (int(centers[index][0]), int(centers[index][1])), int(radius[index]), (0, 0, 255), 5) 
-        cv2.putText(cv2_frame, 'Mission Complete!', (20, 130), font, 2, (0, 0, 255), 5)
+        cv2.putText(cv2_frame, 'GO TO GOAL!', (20, 130), font, 2, (0, 0, 255), 5)
         # controller actions
         linear_vel =  0 #self.linear_vel_control.calculate(1, 174, radius[0])
         angular_vel = self.angular_vel_control.calculate(1, 640, centers[0][0])
@@ -119,6 +127,17 @@ class Camera:
         #print('angular vel: ' + str(angular_vel))
         self.goal_move_base(centers[0][0], radius[0])
         print('##################################')
+        if self.controller_flag:
+          control_input = Twist()
+          control_input.linear.x =  self.xControl.calculate(1, 160, radius[0])
+          control_input.angular.z = self.yawControl.calculate(1, self.camera_info.width/2, centers[0][0])
+          self.velocity_ajustment.publish(control_input)
+      else:
+        self.controller_flag = False  
+        self.xControl.reset()
+        self.yawControl.reset()
+        self.error_distance = 999
+        self.counter = 0
     # merge timer info to frame
     cv2.putText(cv2_frame, str(timer) + 's', (20, 60), font, 2, (50, 255, 50), 5) 
     cv2.putText(cv2_frame, str(time.ctime()), (10, 700), font, 2, (50, 255, 50), 6)
@@ -127,8 +146,8 @@ class Camera:
     ros_frame = self.bridge.cv2_to_imgmsg(cv2_frame, "bgr8")
     self.image_pub.publish(ros_frame)
 
-  def callback_odometry(self, data):
-    self.odometry_data = data
+  # def callback_odometry(self, data):
+  #   self.odometry_data = data
 
   def callback_camera_info(self, data):
     self.camera_info = data
@@ -139,44 +158,77 @@ class Camera:
     # simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-  def cmd_vel_pub(self, linear, angular, frame):
-    cv2.putText(frame, 'Process: center alignment', (20, 640), cv2.FONT_HERSHEY_SIMPLEX, 2, (200, 0, 0), 3)
-    vel_msg = Twist()
-    vel_msg.linear.x = linear
-    vel_msg.angular.z = angular
-    self.velocity_publisher.publish(vel_msg)
+  # def cmd_vel_pub(self, linear, angular, frame):
+  #   cv2.putText(frame, 'Process: center alignment', (20, 640), cv2.FONT_HERSHEY_SIMPLEX, 2, (200, 0, 0), 3)
+  #   vel_msg = Twist()
+  #   vel_msg.linear.x = linear
+  #   vel_msg.angular.z = angular
+  #   self.velocity_publisher.publish(vel_msg)
   
-  def goal_move_base(self, center_ball, radius):
-    distance = (1 * self.focalLength) / (radius * 2)
-    y_move_base = -(center_ball - self.camera_info.width/2) / (radius*2) 
-    if abs(y_move_base) < 0.006:
-      x_move_base = distance
-    else:
-      x_move_base = math.sqrt(distance**2 - y_move_base**2)
-    self.msg_move_to_goal.pose.position.x = x_move_base
-    self.msg_move_to_goal.pose.position.y = y_move_base
-    self.msg_move_to_goal.pose.orientation.w = 1
-    self.msg_move_to_goal.header.frame_id = self.camera_info.header.frame_id
-    if self.flag:
-      self.cancel_explore.publish()
-      os.system("rosnode kill /Operator")
-      print('enviando goal')
-      self.pub_move_to_goal.publish(self.msg_move_to_goal)
-      self.flag = False
-      self.timer_flag = time.time()
-    if time.time() - self.timer_flag > 10:
-      self.flag = True      
-    print('distance to sphere: ' + str(distance))
-    print('INCREMENTO X: ' + str(x_move_base))
-    print('INCREMENTO Y: ' + str(y_move_base))
+  # def goal_move_base(self, center_ball, radius):
+  #   distance = (1 * self.focalLength) / (radius * 2)
+  #   y_move_base = -(center_ball - self.camera_info.width/2) / (radius*2) 
+  #   if abs(y_move_base) < 0.006:
+  #     x_move_base = distance
+  #   else:
+  #     x_move_base = math.sqrt(distance**2 - y_move_base**2)
+  #   self.msg_move_to_goal.pose.position.x = x_move_base
+  #   self.msg_move_to_goal.pose.position.y = y_move_base
+  #   self.msg_move_to_goal.pose.orientation.w = 1
+  #   self.msg_move_to_goal.header.frame_id = self.camera_info.header.frame_id
+  #   if self.flag:
+  #     self.cancel_explore.publish()
+  #     os.system("rosnode kill /Operator")
+  #     print('enviando goal')
+  #     self.pub_move_to_goal.publish(self.msg_move_to_goal)
+  #     self.flag = False
+  #     self.timer_flag = time.time()
+  #   if time.time() - self.timer_flag > 10:
+  #     self.flag = True      
+  #   print('distance to sphere: ' + str(distance))
+  #   print('INCREMENTO X: ' + str(x_move_base))
+  #   print('INCREMENTO Y: ' + str(y_move_base))
 
 
-  def pub_move_base(self, x, y):
-    if self.mission_phase == None:
-      self.mission_phase = 1
+  # def pub_move_base(self, x, y):
+  #   if self.mission_phase == None:
+  #     self.mission_phase = 1
 
   #def move_base_pub(self, x, y, angle):
     #coment
+
+  def goal_move_base(self, center_ball, radius):
+    distance = (1 * self.focalLength) / (radius * 2)
+    y_move_base = -(center_ball - self.camera_info.width/2) / (radius*2) 
+    x_move_base = math.sqrt(distance**2 - y_move_base**2)
+    self.distance_filtered = 0.6*self.distance_filtered + 0.4*distance
+    self.x_move_base_filtered = 0.6*self.x_move_base_filtered + 0.4*x_move_base
+    self.y_move_base_filtered = 0.6*self.y_move_base_filtered + 0.4*y_move_base
+    self.msg_move_to_goal.pose.position.x = x_move_base
+    self.msg_move_to_goal.pose.position.y = y_move_base
+    self.msg_move_to_goal.pose.orientation.w = 1
+    self.msg_move_to_goal.header.frame_id = 'base_link'#self.camera_info.header.frame_id
+    if self.flag and abs(distance- self.distance_filtered) < 7 and self.distance_filtered > 4:
+      self.pub_move_to_goal.publish(self.msg_move_to_goal)
+      self.flag = False
+      self.timer_flag = time.time()
+    if time.time() - self.timer_flag > 5:
+      self.flag = True      
+    if abs(distance- self.distance_filtered) < 1 and self.distance_filtered < 4:
+      self.cancel_move_base.publish()
+      self.controller_flag = True
+
+    print('Euclidean distance to goal: ' + str(distance))
+    print('Filtered euclidean distance to goal: ' + str(self.distance_filtered))
+    print('Error: ' + str(distance - self.distance_filtered))
+    print('X relative to '+ self.camera_info.header.frame_id + ': '  + str(x_move_base))
+    print('Filtered X relative to '+ self.camera_info.header.frame_id + ': '  + str(self.x_move_base_filtered))
+    print('Y relative to '+ self.camera_info.header.frame_id + ': '  + str(y_move_base))
+    print('Filtered Y relative to '+ self.camera_info.header.frame_id + ': '  + str(self.y_move_base_filtered))
+    self.counter += 1
+    self.error_distance = distance - self.distance_filtered
+    self.distance_controller = distance
+    print(str(self.counter))
 
 class Controller:
   sat_max = 0
